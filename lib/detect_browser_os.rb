353cc5264f015523876cb385cc021f51f82f6329
Lars Klevan <lars@socialcast.com>
2011-09-10 13:43:05 -0500

    Add iPad to detect_os

--- lib/detect_browser_os.rb
+++ lib/detect_browser_os.rb
@@ -5,10 +5,22 @@ end
 
 def detect_os(user_agent = request.user_agent)
   ua = (user_agent || "").downcase
-
-  ua.include?('mac') || ua.include?('darwin') ? ua.include?('iphone') ? 'iphone' : ua.include?('ipod') ? 'ipod' : 'mac' :
-  ua.include?('x11') || ua.include?('linux') ? 'linux' :
-  ua.include?('win') ? 'win' : nil
+  
+  if ua.include?('iphone')
+    'iphone'
+  elsif ua.include?('ipad')
+    'ipad'
+  elsif ua.include?('ipod')
+    'ipod'
+  elsif ua.include?('mac') || ua.include?('darwin')
+    'mac'
+  elsif ua.include?('x11') || ua.include?('linux')
+    'linux'
+  elsif ua.include?('win')
+    'win'
+  else
+    nil
+  end
 end
 def detect_browser(user_agent = request.user_agent)
   ua = (user_agent || "").downcase
